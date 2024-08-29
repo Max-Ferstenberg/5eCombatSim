24029455cc7c934992a85b3b5a5f871f271ddcbc
Max-Ferstenberg <maxferstenberg1@gmail.com>
2024-08-29 16:21:17 +0100

    Setup input fields and button names

---
+++ Assets/Scripts/Menu Scripts/MainMenuController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void CreateNewScene() {
+        
+    }
+
+    public void ExitGame(){
+
+    }
+
+    public void ImportGameState(){
+
+    }
+}
